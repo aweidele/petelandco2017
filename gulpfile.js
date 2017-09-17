2841494d07774f996688543e986f06ceba2c2a29
Aaron Weidele <aweidele@gmail.com>
2017-09-17 18:17:17 -0400

    getting gulp and auto prefixer working

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
 
 var autoprefixerOptions = {
-  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+  browsers: ['last 2 versions']
 };
 
 gulp.task('sass', function(){
-  return gulp.src('src/css/main.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+  return gulp
+    .src('src/css/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(gulp.dest('css/'))
 });
