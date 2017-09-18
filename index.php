9a2934a6a0feaf408ba15e948b754de54ba75666
Aaron Weidele <aweidele@gmail.com>
2017-09-17 20:15:05 -0400

    grid setup

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
   get_header();
 ?>
-<div class="test">Hello</div>
+<div class="test">
+  <div class="row">
+    <div class="col_1">1</div>
+    <div class="col_11">11</div>
+  </div>
+  <div class="row">
+    <div class="col_2">2</div>
+    <div class="col_10">10</div>
+  </div>
+  <div class="row">
+    <div class="col_3">3</div>
+    <div class="col_9">9</div>
+  </div>
+  <div class="row">
+    <div class="col_4">4</div>
+    <div class="col_8">8</div>
+  </div>
+  <div class="row">
+    <div class="col_5">5</div>
+    <div class="col_7">7</div>
+  </div>
+  <div class="row">
+    <div class="col_6">6</div>
+    <div class="col_6">6</div>
+  </div>
+</div>
 <?php
   get_footer();
 ?>
