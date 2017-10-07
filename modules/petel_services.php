77ad400075512f96843f2f4badada1e9c7368424
Aaron Weidele <aweidele@gmail.com>
2017-10-07 15:04:33 -0400

    Services page

---
+++ modules/petel_services.php
@@ -0,0 +1,29 @@
+<?php
+  $content = get_field('services',$section->ID);
+  $services = preg_split ('/$\R?^/m', $content['services_list']);
+?>
+<section class="services<?php echo $background_class ? ' '.$background_class : ''?>" id="<?php echo $section->post_name; ?>">
+  <div class="row">
+    <div class="col-11 push-1">
+      <h3 class="services_title"><?php echo $section->post_title; ?></h3>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4 push-1">
+      <div class="services_intro">
+        <?php echo wpautop($content['intro_copy']); ?>
+      </div>
+    </div>
+    <div class="col-6 push-1">
+      <h4 class="services_heading"><?php echo $content['section_heading']; ?></h4>
+      <ul class="services_list">
+<?php foreach($services as $service) { ?>
+        <li class="services_item"><?=$service?></li>
+<?php } ?>
+      </ul>
+      <pre><?php print_r($services); ?></pre>
+    </div>
+  </div>
+<section>
+
+<pre><?php print_r($content); ?></pre>
