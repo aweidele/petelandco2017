6531473ec0f7b9d87cc3430afad89573f4b56ad1
Aaron Weidele <aweidele@gmail.com>
2017-11-12 10:07:28 -0500

    Adding the single project page

--- functions.php
+++ functions.php
@@ -22,6 +22,8 @@ function register_menus() {
 
 // Image Sizes
 add_image_size( 'staff-portait', 480, 480, true );
+add_image_size( 'full', 1200, 999999 );
+add_image_size( 'project-listing', 800, 436, true );
 
 // Register Custom Post Type
 function custom_post_type() {
@@ -57,7 +59,7 @@ function custom_post_type() {
 	);
 	$args = array(
 		'label'                 => __( 'Staff', 'text_domain' ),
-		'description'           => __( 'Defines the colors used in the site', 'text_domain' ),
+		'description'           => __( 'Staff', 'text_domain' ),
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'custom-fields', 'editor', 'page-attributes'),
 		'hierarchical'          => false,
@@ -81,6 +83,62 @@ function custom_post_type() {
 		),
 	);
 	register_post_type( 'petel-staff', $args );
+
+	$labels = array(
+		'name'                  => _x( 'Projects', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Project', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Project', 'text_domain' ),
+		'name_admin_bar'        => __( 'Project', 'text_domain' ),
+		'archives'              => __( '', 'text_domain' ),
+		'attributes'            => __( '', 'text_domain' ),
+		'parent_item_colon'     => __( '', 'text_domain' ),
+		'all_items'             => __( 'All Projects', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Project', 'text_domain' ),
+		'add_new'               => __( 'Add New Project', 'text_domain' ),
+		'new_item'              => __( 'New Project', 'text_domain' ),
+		'edit_item'             => __( 'Edit Project', 'text_domain' ),
+		'update_item'           => __( 'Update Project', 'text_domain' ),
+		'view_item'             => __( 'View Project', 'text_domain' ),
+		'view_items'            => __( 'View Project', 'text_domain' ),
+		'search_items'          => __( 'Search Projects', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( '', 'text_domain' ),
+		'set_featured_image'    => __( '', 'text_domain' ),
+		'remove_featured_image' => __( '', 'text_domain' ),
+		'use_featured_image'    => __( '', 'text_domain' ),
+		'insert_into_item'      => __( '', 'text_domain' ),
+		'uploaded_to_this_item' => __( '', 'text_domain' ),
+		'items_list'            => __( '', 'text_domain' ),
+		'items_list_navigation' => __( '', 'text_domain' ),
+		'filter_items_list'     => __( '', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Projects', 'text_domain' ),
+		'description'           => __( 'Projects and Case Studies', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'custom-fields', 'editor', 'page-attributes'),
+		'hierarchical'          => false,
+		'public'                => false,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-hammer',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => false,
+		'can_export'            => true,
+		'has_archive'           => false,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+		'rewrite'								=> array(
+			'slug'                  => 'project',
+			'with_front'            => true,
+			'pages'                 => true,
+			'feeds'                 => true,
+		),
+	);
+	register_post_type( 'projects', $args );
 
 	flush_rewrite_rules();
 
---
+++ single-projects.php
@@ -0,0 +1,50 @@
+<?php
+  get_header();
+  if( have_posts() ) : while( have_posts() ) : the_post();
+?>
+<?php if($post->post_content) {
+  $fields = get_fields();
+  $background_class = 'bg-'.get_field('theme_theme');
+?>
+  <article class="project">
+  <div class="project_info<?php echo $background_class ? ' '.$background_class : ''?>">
+    <div class="row">
+      <header class="project_header col-3 push-1">
+        <h1 class="project_title"><?php echo $post->post_title; ?></h1>
+        <?php if($fields['subtitle']) { ?>
+        <div class="project_subtitle"><?php echo $fields['subtitle']; ?></div>
+        <?php } ?>
+      </header>
+      <div class="col-8">
+        <div class="project_info_content">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php } else { ?>
+  <header class="page_header">
+    <div class="row">
+      <h1 class="page_title"><?php echo $post->post_title; ?></h1>
+    </div>
+  </header>
+<?php } ?>
+  <main class="project_images">
+    <div class="row">
+<?php foreach($fields['gallery'] as $image) { ?>
+      <figure class="project_image">
+        <img src="<?php echo $image['sizes']['full']; ?>">
+      </figure>
+<?php } ?>
+    </div>
+  </main>
+  </article>
+<?php /*
+<pre><?php print_r($fields); ?></pre>
+<pre><?php print_r($post); ?></pre>
+*/ ?>
+<?php
+  endwhile;
+  endif;
+  get_footer();
+?>
