d095d6a24bb475c13e972f4cd829ae978da97edd
Aaron Weidele <aweidele@gmail.com>
2018-04-25 10:41:12 -0400

    add easydropdown

--- src/js/jquery.easydropdown.js
+++ src/js/jquery.easydropdown.js
@@ -477,30 +477,3 @@
 		});
 	});
 })(jQuery);
-var $ = jQuery;
-$(document).ready(function() {
-
-  if($(".tabbed_content_tabs").length) {
-    $(".tabbed_content_tab").on("click",function(e) {
-      e.preventDefault();
-      var h = $(this).attr("href");
-
-      $(this).siblings().removeClass("active");
-      $(this).addClass("active");
-
-      $(".tabbed_content_item").removeClass("active");
-      $(h).addClass("active");
-    });
-  }
-
-  $(".tabbed_content_item").on("click",function() {
-    var t = "#" + $(this).attr("aria-labelledby");
-
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-
-    $(".tabbed_content_tab").removeClass("active");
-    $(t).addClass("active");
-  });
-
-});
