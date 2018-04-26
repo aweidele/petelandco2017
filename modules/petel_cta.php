067169c7d3151c9e4d2b9a078e55fa5782e9f0df
Aaron Weidele <aweidele@gmail.com>
2018-04-26 07:44:47 -0400

    cta button

--- modules/petel_cta.php
+++ modules/petel_cta.php
@@ -1,10 +1,15 @@
+<?php
+  $cta_statement = get_field("call_to_action_statement","option");
+  $cta_label = get_field("call_to_action_button_label","option");
+  $cta_link = get_field("call_to_action_link","option");
+?>
 <aside class="cta theme_dk_teal">
   <div class="row">
     <div class="col-6 push-1">
-      <div class="cta_message">Call to action statement goes here</div>
+      <div class="cta_message"><?=$cta_statement?></div>
     </div>
     <div class="col-4">
-      <a class="cta_link" href="">Contact Us<?php echo icon("double_chevron_right"); ?></a>
+      <a class="cta_link" href="<?=$cta_link["url"]?>"<?php if($cta_link["target"]) { echo ' target="_blank"'; } ?>><?=$cta_label?><?php echo icon("double_chevron_right"); ?></a>
     </div>
   </div>
 </aside>
---
+++ modules/petel_cta_button.php
@@ -0,0 +1,8 @@
+<?php
+  $background = $callout["cta_button_color"];
+?>
+<section class="cta_button theme_<?=$background?>">
+  <div class="row">
+    <a href="<?=$callout["cta_link"]?>" class="cta_button_link"><?=$callout["cta_text"]?> <?=icon("double_chevron_right");?></a>
+  </div>
+</section>
