73323b400ce41469f4028690eb29b6520cd9a238
Aaron Weidele <aweidele@gmail.com>
2018-06-05 18:12:51 -0600

    Server side

--- template-contact.php
+++ template-contact.php
@@ -1,7 +1,41 @@
 <?php
 /* Template Name: Contact */
+$address = get_field('address','option');
+$address_line_2 = get_field('address_line_2','option');
+$city = get_field('city','option');
+$state = get_field('state','option');
+$zip = get_field('zip','option');
+$phone = get_field('phone','option');
+$email = get_field('email','option');
+
 get_header();
 ?>
-Hi.
+<header class="page_header">
+  <div class="row">
+    <h2 class="page_title"><?php echo $post->post_title; ?></h2>
+  </div>
+</header>
+<main class="contact_main">
+  <div class="row">
+    <div class="col-4">
+      <div class="contact_wrapper">
+        <a href="https://www.google.com/maps/place/<?=urlencode($address.' '.$address_line_2.' '.$city.' '.' '.$state.' '.$zip)?>/" target="_blank">
+          <?php echo icon('map'); ?>
+          <address class="contact_address"><?=$address?><br>
+          <?php if($address_line_2) { ?>
+          <?=$address_line_2?><br>
+          <?php } ?>
+          <?=$city?>, <?=$state?> <?=$zip?></address>
+        </a>
+        <a href="tel:<?=preg_replace("/[^0-9]/", "", $phone )?>">
+        <?php echo icon('phone'); ?>
+        <?=$phone?></a>
+      </div>
+    </div>
+    <div class="col-8">
+      contact form
+    </div>
+  </div>
+</main>
 <?php
 get_footer();
